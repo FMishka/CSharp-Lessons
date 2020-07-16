9026d0aaadeb9c781b3a85eccd369b26bb4b580b
FMishka <51850577+FMishka@users.noreply.github.com>
2020-07-17 02:23:59 +0300

    Add files via upload

---
+++ oop/oop/NumberManipulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class NumberManipulator
+    {
+        public int findMax(int firstNumber, int secondNumber)
+        {
+            if (firstNumber > secondNumber)
+            {
+                return firstNumber;
+            }
+            else if (firstNumber < secondNumber)
+            {
+                return secondNumber;
+            }
+            else
+            {
+                return firstNumber;
+            }
+        }
+    }
+}
---
+++ oop/oop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Поиск максимума
+            int firstNumber, secondNumber;
+            Console.WriteLine("Введите первое число");
+            firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            secondNumber = int.Parse(Console.ReadLine());
+            NumberManipulator numb = new NumberManipulator();
+            Console.WriteLine(numb.findMax(firstNumber, secondNumber));
+            //Объявление человека
+            Human man = new Human();
+            man.Name = "Tom";
+            man.Age = 21;
+            Console.WriteLine("Name: " + man.Name + "\nAge: " + man.Age.ToString());
+
+        }
+    }
+}
