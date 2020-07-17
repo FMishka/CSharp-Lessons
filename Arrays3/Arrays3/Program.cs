608b66a38c3c42b7e5566d0ea942af06fb8bedee
FMishka <51850577+FMishka@users.noreply.github.com>
2020-07-17 03:10:23 +0300

    Add files via upload

---
+++ Arrays3/Arrays3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] myArray = new int[3, 4, 5];
+            Random randint = new Random();
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < myArray.GetLength(2); k++)
+                    {
+                        myArray[i, j, k] = randint.Next(100);
+                    }
+                }
+            }
+            foreach (var item in myArray)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
