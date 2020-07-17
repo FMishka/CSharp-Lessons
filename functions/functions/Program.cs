4c385f9d593cae9d1dab0853111651459a339771
FMishka <51850577+FMishka@users.noreply.github.com>
2020-07-17 17:03:21 +0300

    Add files via upload

---
+++ functions/functions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions
+{
+    class Program
+    {
+        static int Sum(int first, int second)
+        {
+            return first + second;
+        }
+
+        static void Sum1(int first, int second)
+        {
+            Console.WriteLine(first + second);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b = int.Parse(Console.ReadLine());
+            int result = Sum(a, b);
+            Console.WriteLine("S = " + result);
+            a *= 2;
+            b *= 2;
+            Console.WriteLine("a = " + a + " b = " + b);
+            Sum1(a, b);
+        }
+    }
+}
---
+++ functions/functions1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions1
+{
+    class Program
+    {
+        static void PrintStr(string symbols, uint count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(symbols);
+            }
+        }
+        static int SearchNumb(int numb, int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == numb)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            //Создание строки
+            Console.WriteLine("Введите символы");
+            string str = Console.ReadLine();
+            Console.WriteLine("Введите количество повторов этих символов");
+            uint countStr = uint.Parse(Console.ReadLine());
+            PrintStr(str, countStr);
+            //Поиск первого индекса элемента
+            Console.WriteLine();
+            Console.WriteLine("Введите значение элемента");
+            int[] myArray = { 2, 3, 4, 6 , 7, 8, 9, 9, 9, 4};
+            int number = int.Parse(Console.ReadLine());
+            int result = SearchNumb(number, myArray);
+            Console.WriteLine(result);
+        }
+    }
+}
