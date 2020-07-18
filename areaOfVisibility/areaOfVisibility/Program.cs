162bfec0fd52432f120652245136be285da489c1
FMishka <51850577+FMishka@users.noreply.github.com>
2020-07-18 03:13:17 +0300

    Add files via upload

---
+++ areaOfVisibility/areaOfVisibility/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace areaOfVisibility
+{
+    class Program
+    {
+        static int test = 12;
+        static void Test()
+        {
+            int test = 13;
+            Console.WriteLine(test);
+        }
+
+        int a = 555;
+        static int g = 123;
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+            int b = test.a;
+            Console.WriteLine(b);
+            Console.WriteLine(g);
+            Test();
+        }
+    }
+}
