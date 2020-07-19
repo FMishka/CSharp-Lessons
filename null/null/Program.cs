dc2bd0097d2a1db171a0e8c17fb91a45846fe900
FMishka <51850577+FMishka@users.noreply.github.com>
2020-07-19 03:45:28 +0300

    Add files via upload

---
+++ null/null/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @null
+{
+    class Person
+    {
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+        public string middleName { get; set; }
+
+
+        public string GetFullName()
+        {
+            return $"Фамилия: {secondName}, Имя: {firstName}, Отчество: {middleName ?? "Нет данных"}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = null;
+            Console.WriteLine(str ?? "Нет данных");
+            Person person = new Person() {firstName = "Иван", secondName = "Иванов", middleName = "Иванович"};
+            Console.WriteLine(person.GetFullName());
+            Person person1 = new Person() { firstName = "Иван", secondName = "Иванов"};
+            Console.WriteLine(person1.GetFullName());
+        }
+    }
+}
