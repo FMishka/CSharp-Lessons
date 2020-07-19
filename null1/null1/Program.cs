39ee51f302d9d33932a63fb8ce289f2891fd5a27
FMishka <51850577+FMishka@users.noreply.github.com>
2020-07-19 04:11:22 +0300

    Add files via upload

---
+++ null1/null1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace null1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Оператор "??=" не работает на .NET Framework 4.7.2
+            string str = null;
+            str ??= string.Empty;
+            Console.WriteLine(str.Length);
+            string str1 = "text";
+            str1 ??= string.Empty;
+            Console.WriteLine(str1.Length);
+        }
+    }
+}
