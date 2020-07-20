cce09699501e50116db26368a173a25964ec3342
FMishka <51850577+FMishka@users.noreply.github.com>
2020-07-20 04:01:20 +0300

    Add files via upload

---
+++ null2/null2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace null2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[3] { 1, 2, 3};
+            int[] myArray1 = null;
+            Console.WriteLine("Кол-во элементов в первом массиве: " + (myArray?.Length ?? 0));
+            Console.WriteLine("Кол-во элементов в первом массиве: " + (myArray1?.Length ?? 0));
+        }
+    }
+}
