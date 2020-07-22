652aef05966b4fb9ec54d3efaf735e61618b44bf
FMishka <51850577+FMishka@users.noreply.github.com>
2020-07-22 04:15:33 +0300

    Add files via upload

---
+++ arraySort/arraySort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraySort
+{
+    class Program
+    {
+        static void BoobleSort(ref int[] array)
+        {
+            int space;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        space = array[j];
+                        array[j] = array[i];
+                        array[i] = space;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Пузарьковый метод сортировки
+            int[] myArray = new int[12];
+            Random rand = new Random();
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = rand.Next(10);
+            }
+            BoobleSort(ref myArray);
+            foreach (var item in myArray)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
