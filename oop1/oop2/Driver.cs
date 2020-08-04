0827f3590500f61729dac30a059d1978dd88d1d7
FMishka <51850577+FMishka@users.noreply.github.com>
2020-08-04 03:24:43 +0300

    Add files via upload

---
+++ oop1/oop2/Driver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+    class Driver : Human
+    {
+        public void checkInt()
+        {
+            if (getInt <= 80)
+            {
+                Console.WriteLine(Name + " - немного туповат");
+            }
+            else
+            {
+                Console.WriteLine(Name + " - умный");
+            }
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("ПОИХАЛЫ!!!1!!1");
+        }
+    }
+}
