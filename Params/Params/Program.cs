d91fdab766793d31affa732b54773a63574a98b9
FMishka <51850577+FMishka@users.noreply.github.com>
2020-08-20 16:14:13 +0300

    Add files via upload

---
+++ Params/Params/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    class Program
+    {
+        static int Sum(params int[] parameters)
+        {
+            int result = 0;
+            for (int i = 0; i< parameters.Length; i++)
+            {
+                result += parameters[i];
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int a = Sum(1,2,3,6,7,7,7,7,8);
+            Console.WriteLine(a);
+        }
+    }
+}
