7334d0f118269a90554866aaa8112fdcd49237ba
FMishka <51850577+FMishka@users.noreply.github.com>
2020-08-22 16:19:55 +0300

    Add files via upload

---
+++ ClassCont/ClassCont/Oleg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCont
+{
+    static class Oleg
+    {
+        //По умолчанию стоит private
+        public static class Name
+        {
+            private static string FirstName()
+            {
+                return "Олег";
+            }
+            private static string SecondName()
+            {
+                return "Алексеев";
+            }
+            public static string FullName()
+            {
+                return $"{FirstName()} {SecondName()}";
+            }
+        }
+        public static class Body
+        {
+            public static int ArmCount()
+            {
+                return 2;
+            }
+            public static int LegCount()
+            {
+                return 2;
+            }
+            public static int HeadCount()
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ ClassCont/ClassCont/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ivanov;
+//"Псевдоним" для пространства имён "Petrovich"
+using P = Petrovich.Petr;
+
+namespace ClassCont
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Ivanov.Ivan.Name());
+            Console.WriteLine(Ivanov.Petr.Name());
+            //Сокращение название класса на строке 7
+            Console.WriteLine(P.Name());
+            //Работает из-за строки 6
+            Console.WriteLine(Ivan.Name());
+            Console.WriteLine(Petr.Name());
+            Console.WriteLine();
+            Console.WriteLine(Oleg.Name.FullName());
+        }
+    }
+}
+//Пример
+//Приписка "internal" означает, что классы видны только в одном проекте (если на его месте ничего нет, то считается что по умолчанию стоит internal)
+namespace Ivanov
+{
+    internal static class Ivan
+    {
+        public static string Name()
+        {
+            return "Иванов Иван";
+        }
+    }
+
+    public static class Petr
+    {
+        public static string Name()
+        {
+            return "Иванов Пётр";
+        }
+    }
+}
+
+namespace Petrovich
+{
+    public static class Petr
+    {
+        public static string Name()
+        {
+            return "Петрович Пётр";
+        }
+    }
+}
