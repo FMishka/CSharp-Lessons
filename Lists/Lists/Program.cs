1e01d114e8fc67f55f3c6ab21aa1307ffa6c1070
FMishka <51850577+FMishka@users.noreply.github.com>
2020-08-28 21:09:39 +0300

    Add files via upload

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Списки
+            List<int> myList = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                myList.Add(i);
+            }
+            foreach (var item in myList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(myList[1]);
+            //Кортежи
+            var tuple = (10, 11, "23");
+            Console.WriteLine(tuple);
+            var tuple1 = (first:10, second:11, third:"23");
+            Console.WriteLine(tuple1.first);
+            var (name, number) = ("Jhon", 123);
+            Console.WriteLine(name);
+        }
+    }
+}
