fbd3c1ee6675d50074b0761f1ec61ac0b754fbd0
FMishka <51850577+FMishka@users.noreply.github.com>
2020-11-23 22:47:26 +0300

    Add files via upload

---
+++ Transform/Transorm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Transorm
+{
+    class Program
+    {
+        static void Foo(float value)
+        {
+            Console.WriteLine(value);
+        }
+        static void Main(string[] args)
+        {
+            double a = 5.3;
+            float b = (float)a;
+            Foo(b);
+            int aa = 257;
+            //Расширяющее преобразование
+            float bb = aa;
+            byte q = (byte)aa;
+            bool w = Convert.ToBoolean(a);
+
+            float first = 2.5f;
+            int second = 4;
+
+            int result = (int)(second +first);
+            result = second + (int)first;
+        }
+    }
+}
