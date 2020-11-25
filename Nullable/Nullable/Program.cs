f41f3c168854a1e37f56eb69a8121fef0e1872cc
FMishka <51850577+FMishka@users.noreply.github.com>
2020-11-25 15:49:24 +0300

    Add files via upload

---
+++ Nullable/Nullable/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            // Знак вопроса создаёт nullable-тип
+            int? a = null;
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a == null);
+            Console.WriteLine(a.GetValueOrDefault());
+            Console.WriteLine(a ?? 100);
+            try
+            {
+                Console.WriteLine(a.Value);
+            }
+            catch(InvalidOperationException)
+            {
+                Console.WriteLine("null");
+            }
+            Console.WriteLine(a);
+
+            int? b = 20;
+            Console.WriteLine(b.HasValue);
+            Console.WriteLine(b == null);
+            Console.WriteLine(b.GetValueOrDefault());
+            Console.WriteLine(b ?? 100);
+            try
+            {
+                Console.WriteLine(b.Value);
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("null");
+            }
+            Console.WriteLine(b);
+
+            int? result = a + b;
+            try
+            {
+                Console.WriteLine(result.Value);
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Result = null");
+            }
+            Console.WriteLine(a<b);
+            Console.WriteLine(a==b);
+            Console.WriteLine(a>b);
+        }
+    }
+}
