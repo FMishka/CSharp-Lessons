60b1a58748136daf7922a529f86bb0ca7d739d08
FMishka <51850577+FMishka@users.noreply.github.com>
2021-01-20 03:34:11 +0300

    Add files via upload

---
+++ Enum1/Enum1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Enum1
+{
+    class Program
+    {
+        enum Days
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+                int keyCode = (int)key;
+                Console.WriteLine($"\tEnum {key}\tKey Code {keyCode}");
+                if (key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Enter is pressed!");
+                }
+            }
+
+            int value = 5;
+            Days day;
+
+            if (Enum.IsDefined(typeof(Days), value) == true)
+            {
+                day = (Days)value;
+            }
+            else
+            {
+                throw new Exception("Invalid Day value!");
+            }
+            var allDays = Enum.GetValues(typeof(Days));
+
+            foreach (var item in allDays)
+            {
+                Console.WriteLine($"{item} ");
+            }
+            Console.WriteLine();
+
+            string enter = Console.ReadLine();
+            Days nowDay = (Days)Enum.Parse(typeof(Days), enter, ignoreCase: true);
+            Console.WriteLine(nowDay);
+        }
+    }
+}
