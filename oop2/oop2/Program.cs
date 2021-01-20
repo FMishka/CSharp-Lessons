bdc70c9407bd42a412a5cc269e4539e67546c5e4
FMishka <51850577+FMishka@users.noreply.github.com>
2021-01-20 17:01:31 +0300

    Add files via upload

---
+++ oop2/oop2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace oop2
+{
+    enum Color
+    {
+        Red,
+        Green,
+        Orange,
+        Yellow,
+        Blue
+    }
+    class Point
+    {
+        public int x;
+        public int y;
+        public Color color;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point();
+            p.x = 4;
+            p.y = 5;
+            p.color = Color.Orange;
+            Console.WriteLine($"X = {p.x}, Y = {p.y}, Color = {p.color}");
+            p = null; // Разрыв связи с классом
+        }
+    }
+}
