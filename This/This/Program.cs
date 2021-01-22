7d39c5871b0bc827f41ea04a74fe200dc9700af4
FMishka <51850577+FMishka@users.noreply.github.com>
2021-01-23 01:26:40 +0300

    Add files via upload

---
+++ This/This/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace This
+{
+    class Human
+    {
+        public Human(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public Human(string name, int age, string gender) : this(name, age)
+        {
+            this.gender = gender;
+        }
+        private string name;
+        private int age;
+        private string gender;
+        public void PrintHuman()
+        {
+            Console.WriteLine($"Name: {name}\nAge: {age}\nGender: {gender}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human man = new Human("Misha", 18, "Man");
+            man.PrintHuman();
+        }
+    }
+}
