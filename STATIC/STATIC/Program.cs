fce47c6648cea3c61fff853553e0cc05e91efe85
FMishka <51850577+FMishka@users.noreply.github.com>
2021-01-25 01:28:54 +0300

    Add files via upload

---
+++ STATIC/STATIC/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace STATIC
+{
+    class MyClass
+    {
+        public int a;
+        public static int b;
+        private static int c;
+        public void Func(int a, int c)
+        {
+            this.a = a + 2;
+            MyClass.c = c * c + 2 * c + 2;
+        }
+        public void PrintC()
+        {
+            Console.WriteLine(c);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass class2 = new MyClass();
+            MyClass class1 = new MyClass();
+            class1.a = 55;
+            MyClass.b = 89;
+            class1.Func(2, 23);
+            class2.PrintC();
+        }
+    }
+}
