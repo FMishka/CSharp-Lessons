23c8bfe05ffc192ab6044a8097248288ff8f7e18
FMishka <51850577+FMishka@users.noreply.github.com>
2021-02-02 01:58:31 +0300

    Add files via upload

---
+++ StaticConstructor/StaticConstructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StaticConstructor
+{
+    class Class
+    {
+        public Class()
+        {
+            Console.WriteLine("Конструктор");
+        }
+        static Class() //Всегд выполняется только один раз. Всегда вызывается первым.
+        {
+            Console.WriteLine("Статический конструктор");
+        }
+        static public void Test()
+        {
+            Console.WriteLine("Test!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class.Test();
+
+        }
+    }
+}
