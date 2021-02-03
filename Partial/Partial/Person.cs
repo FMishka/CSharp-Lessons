abd771c7d03076938400f092a21d067240176a89
FMishka <51850577+FMishka@users.noreply.github.com>
2021-02-03 16:10:14 +0300

    Add files via upload

---
+++ Partial/Partial/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Partial
+{
+    partial class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public partial string GetFullName();
+    }
+}
---
+++ Partial/Partial/PersonMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Partial
+{
+    partial class Person
+    {
+        public partial string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+        public void PrintFullName()
+        {
+            Console.WriteLine(GetFullName());
+        }
+    }
+}
