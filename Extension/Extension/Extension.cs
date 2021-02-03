fc38b9d52a7e51c3b12eb9555f7bf6d414157cc9
FMishka <51850577+FMishka@users.noreply.github.com>
2021-02-03 03:11:15 +0300

    Add files via upload

---
+++ Extension/Extension/Extension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyExtension
+{
+    static class Extension
+    {
+        static public void Print(this DateTime dateTime)
+        {
+            Console.WriteLine(dateTime);
+        }
+        static public bool IsWeekend(this DateTime dateTime, DayOfWeek dayOfWeek)
+        {
+            if (DayOfWeek.Saturday == dayOfWeek || DayOfWeek.Sunday == dayOfWeek)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Extension/Extension/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyExtension;
+
+namespace Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            dateTime.Print();
+            Console.WriteLine(dateTime.IsWeekend(dateTime.DayOfWeek));
+        }
+    }
+}
