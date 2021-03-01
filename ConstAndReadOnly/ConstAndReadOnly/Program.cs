ff6d916430953a507d5d7824d742325e7d0eff75
FMishka <51850577+FMishka@users.noreply.github.com>
2021-03-01 15:53:10 +0300

    Add files via upload

---
+++ ConstAndReadOnly/ConstAndReadOnly/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConstAndReadOnly
+{
+    class SomeClass
+    {
+        public const int a = 15; // Неявно статическое поле. Объявляется сразу
+        public readonly int b; // Объявляется только в конструкторе
+        public static readonly int bb; // Явно стаатическое поле.
+        public SomeClass()
+        {
+            b = 14;
+        }
+        static SomeClass()
+        {
+            bb = 13;
+        }
+        public static void PrintA()
+        {
+            Console.WriteLine(a);
+        }
+        public void PrintB()
+        {
+            Console.WriteLine(b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arg = new SomeClass();
+            SomeClass.PrintA();
+            Console.WriteLine(SomeClass.bb);
+            arg.PrintB();
+        }
+    }
+}
