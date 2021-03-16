01ef997d20ddb10a852dec1758e67bfee2a6b339
FMishka <51850577+FMishka@users.noreply.github.com>
2021-03-17 01:44:58 +0300

    Add files via upload

---
+++ Protected/Protected/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Protected
+{
+    class A
+    {
+        public int publicField;
+        private int privateField;
+        protected int protectedField;
+
+        public void PrintFiels()
+        {
+            Console.WriteLine("Public = " + publicField);
+            Console.WriteLine("Protected = " + protectedField);
+            Console.WriteLine("Private = " + privateField);
+        }
+        public A()
+        {
+            Console.WriteLine("Class A\n");
+            Console.WriteLine("Public = " + publicField);
+            Console.WriteLine("Protected = " + protectedField);
+            Console.WriteLine("Private = " + privateField);
+        }
+    }
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("\nClass B\n");
+            Console.WriteLine("Public = " + publicField);
+            //Console.WriteLine(privateField) - НЕДОСТУПЕН
+            Console.WriteLine("Protected = " + protectedField);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            Console.WriteLine();
+            b.PrintFiels();
+        }
+    }
+}
