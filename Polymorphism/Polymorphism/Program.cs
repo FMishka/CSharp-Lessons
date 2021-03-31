0a4b61cb6ddefa6ae8ffe35e13eb643af6f4b02d
FMishka <51850577+FMishka@users.noreply.github.com>
2021-03-31 14:07:30 +0300

    Add files via upload

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Car
+    {
+        public virtual void Drive()
+        {
+            Console.WriteLine("Едет обычная машина");
+        }
+    }
+    class SportCar : Car
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Едет спортивная машина");
+        }
+    }
+    class Person
+    {
+        public void Drive(Car car)
+        {
+            car.Drive();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Drive(new SportCar());
+        }
+    }
+}
