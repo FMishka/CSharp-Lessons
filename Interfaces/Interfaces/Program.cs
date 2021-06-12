b43ca487e2a864f4bb7cebd8626879416c73d029
FMishka <51850577+FMishka@users.noreply.github.com>
2021-06-12 21:03:41 +0300

    Add files via upload

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface IDataProvider
+    {
+        public string GetData();
+    }
+    interface IDataProcessor
+    {
+        public void ProcessData(IDataProvider dataProvider);
+    }
+
+    class ConsoleDataProcessor : IDataProcessor
+    {
+        public void ProcessData(IDataProvider dataProvider)
+        {
+            Console.WriteLine(dataProvider.GetData());
+        }
+    }
+
+    class DbDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Данные из бд";
+        }
+    }
+
+    class FileDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Данные из файла";
+        }
+    }
+
+    class APIDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Данные из апи";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataProcessor dataProcessor = new ConsoleDataProcessor();
+            dataProcessor.ProcessData(new DbDataProvider());
+            dataProcessor.ProcessData(new FileDataProvider());
+            dataProcessor.ProcessData(new APIDataProvider());
+        }
+    }
+}
