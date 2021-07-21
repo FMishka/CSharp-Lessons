f87d5c06e812f336983ef81f3ed0effb48a175c0
FMishka <51850577+FMishka@users.noreply.github.com>
2021-07-21 15:49:42 +0300

    Add files via upload

---
+++ Interface Inheritance/Interface Inheritance/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Interface_Inheritance
+{
+    interface IWeapon
+    {
+        void Fire();
+    }
+
+    interface IThrowingWeapon : IWeapon
+    {
+        void Throw();
+    }
+
+    class Player
+    {
+        public void Fire(IWeapon weapon)
+        {
+            weapon.Fire();
+        }
+
+        public void Throw(IThrowingWeapon weapon)
+        {
+            weapon.Throw();
+        }
+    }
+
+    class Gun : IWeapon
+    {
+        public void Fire()
+        {
+            Console.WriteLine("*Выстрел из пушки*");
+        }
+    }
+
+    class Pistol : IWeapon
+    {
+        public void Fire()
+        {
+            Console.WriteLine("*Выстрел из пистолета*");
+        }
+    }
+
+    class Knife : IThrowingWeapon
+    {
+        public void Fire()
+        {
+            Console.WriteLine("*Атака ножом*");
+        }
+
+        public void Throw()
+        {
+            Console.WriteLine("*Кидание ножа*");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            IWeapon[] inventory = { new Gun(), new Pistol(), new Knife()};
+            foreach (var item in inventory)
+            {
+                player.Fire(item);
+            }
+            player.Throw(new Knife());
+        }
+    }
+}
