936a8541c749260788277a0834d483efbe6bf7cd
FMishka <51850577+FMishka@users.noreply.github.com>
2021-07-27 15:26:19 +0300

    Add files via upload

---
+++ InterfaceС8.0/InterfaceC8.0/ILogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceC8._0
+{
+    interface ILogger
+    {
+        void Log(LogLevel logLevel, string message);
+
+        void Foo()
+        {
+            Console.WriteLine("Реализация в интрефейсе");
+        }
+    }
+}
---
+++ InterfaceС8.0/InterfaceC8.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InterfaceC8._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new ConsoleLogger();
+            logger.Log(LogLevel.Debug, "Debug");
+            logger.Log(LogLevel.Info, "Info");
+            logger.Log(LogLevel.Warning, "Warning");
+            logger.Log(LogLevel.Error, "Error");
+            logger.Log(LogLevel.Fatal, "Fatal");
+            logger.Foo();
+        }
+    }
+}
