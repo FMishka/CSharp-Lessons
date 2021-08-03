3016527e6baf9f83e0552fc9db1f2512f6b35a91
FMishka <51850577+FMishka@users.noreply.github.com>
2021-08-03 12:39:08 +0300

    Add files via upload

---
+++ Structures2/Structures2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Structures2
+{
+    interface IPoint
+    {
+        void Print();
+    }
+    public class ClassPoint : IPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public void Print()
+        {
+            Console.WriteLine("ClassPoint");
+            Console.WriteLine($"X = {X}\nY = {Y}");
+        }
+    }
+    public struct StructPoint : IPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public void Print()
+        {
+            Console.WriteLine("StructPpoint");
+            Console.WriteLine($"X = {X}\nY = {Y}");
+        }
+    }
+    class Program
+    {
+        static void ForClass(ClassPoint point)
+        {
+            point.X++;
+            point.Y++;
+        }
+        static void ForStruct(StructPoint point)
+        {
+            point.X++;
+            point.Y++;
+        }
+        static void Main(string[] args)
+        {
+            ClassPoint classPoint = new ClassPoint();
+            StructPoint structPoint = new StructPoint(); //Заполнение полей стандартными значениями
+
+            ForClass(classPoint);
+            ForStruct(structPoint); // Чтобы изменить значения нужно либо передать по ссылке, либо вернуть значение
+            classPoint.Print();
+            structPoint.Print();
+        }
+    }
+}
+//Структуры для небольших типов данных, которым полиморфизм не нужен
+//В остальных случаях лучше использовать классы
